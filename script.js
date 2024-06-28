180cf8c5d68c2e1837ab916b2d95b43d9adb81f4
Walisson Vilela <59664662+Walisson-Vilela@users.noreply.github.com>
2024-06-28 16:05:56 -0300

    [STYLE] = Ajustes no codigo
    Ajustes no codigo

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-const animais = document.getElementById('animais');
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-console.log(animais);
+if(tabMenu.length && tabContent.length) {
+  tabContent[0].classList.add('ativo');
+
+  function activeTab(index) {
+    tabContent.forEach((section) => {
+      section.classList.remove('ativo');
+    });
+    tabContent[index].classList.add('ativo');
+  }
+
+  tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener('click', () => {
+      activeTab(index);
+    });
+  });
+}
